f03539bd2c48088da3f253526addd17285bd3786
KaioZaniboni <blackdinamo@hotmail.com>
2023-04-26 19:36:28 -0300

    correção em produto-service

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -18,6 +18,7 @@ export class ProdutosComponent implements OnInit {
 
   ngOnInit(): void {
     const produtos = this.produtosService.getAll();
+    
     this.route.queryParamMap.subscribe(params => {
       const descricao = params.get("descricao")?.toLowerCase();
 
